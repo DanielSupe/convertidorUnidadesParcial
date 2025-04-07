92f4d47660e30a5e95a0a724f6db31ec3b8f0498
DanielSupe <daniel.supelanos@unac.edu.co>
2025-04-07 18:34:47 -0500

    cleanCode

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class Convertidor extends StatefulWidget {
 class _Convertidor extends State<Convertidor> {
   int actualPage = 0;
 
-  final List<Map<String, dynamic>> varMap = [
+  final List<Map<String, dynamic>> mapeado = [
   {'title': 'Celsius <> Rankine', 'Color': Colors.green, 'component': Convert(color: Colors.green,function: convertirCelsiusRankine, unit1: "Celsius", unit2: "Rankine")},
   {'title': 'Años luz <> Unidad astronómica', 'Color': Colors.red, 'component': Convert(color: Colors.red,function: convertirAnosLuzAU, unit1: "Años luz", unit2: "Unidad astronómica")},
   {'title': 'Metro <> Milla náutica', 'Color': Colors.blue, 'component': Convert(color: Colors.blue,function: convertirMetroMillaNautica, unit1: "Metro", unit2: "Milla náutica")},
@@ -38,8 +38,8 @@ class _Convertidor extends State<Convertidor> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(varMap[actualPage]['title'],style: TextStyle(color: Colors.white)),
-        backgroundColor:  varMap[actualPage]['Color'],
+        title: Text(mapeado[actualPage]['title'],style: TextStyle(color: Colors.white)),
+        backgroundColor:  mapeado[actualPage]['Color'],
       ),
       drawer: Drawer(
         child: ListView(
@@ -47,17 +47,17 @@ class _Convertidor extends State<Convertidor> {
             Container(
               height: 70,
               child: DrawerHeader(
-                decoration: BoxDecoration(color: varMap[actualPage]['Color']),
+                decoration: BoxDecoration(color: mapeado[actualPage]['Color']),
                 child: Text(
                   'Categorías',
                   style: TextStyle(color: Colors.white, fontSize: 24),
                 ),
               ),
             ),
-            for (int i = 0; i < varMap.length; i++)
+            for (int i = 0; i < mapeado.length; i++)
               ListTile(
                 tileColor: actualPage == i ? const Color.fromARGB(255, 218, 216, 216) : null,
-                title: Text(varMap[i]['title'],),
+                title: Text(mapeado[i]['title'],),
                 onTap: () {
                   setState(() {
                     actualPage = i;
@@ -68,7 +68,7 @@ class _Convertidor extends State<Convertidor> {
           ],
         ),
       ),
-      body: varMap[actualPage]['component'],
+      body: mapeado[actualPage]['component'],
     );
   }
 }
